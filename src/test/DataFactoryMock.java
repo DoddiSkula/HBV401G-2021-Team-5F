64bcd189ba9d2752468da0c68d556b226f9d0cb8
Doddi <doddiskula@gmail.com>
2021-03-28 22:42:11 +0000

    Test fixture

--- src/test/DataFactoryMock.java
+++ src/test/DataFactoryMock.java
@@ -9,14 +9,31 @@ import javafx.collections.ObservableList;
 
 public class DataFactoryMock implements DataFactoryInterface {
 
-    // Munum ekki nota testi
+    /**
+     * Sækir alla notendur.
+     *
+     * @return listi af notendum
+     */
     public ObservableList<User> getUsers() {
-        return null;
+        ObservableList<User> users = FXCollections.observableArrayList();
+        users.add(new User("Matthias Book", "mb123@hi.is", "12345678"));
+        users.add(new User("John Smith", "johnny@hi.is", "12345678"));
+        return users;
     }
 
-    // Munum ekki nota í testi
+    /**
+     * Sækir öll sæti í tilteknu flugi.
+     *
+     * @param flight_id flugnúmer
+     * @return listi af sætum
+     */
     public ObservableList<Seat> getSeats(int flight_id) {
-        return  null;
+        ObservableList<Seat> seats = FXCollections.observableArrayList();
+        seats.add(new Seat(1, 1, true, false, false));
+        seats.add(new Seat(1, 2, true, false, false));
+        seats.add(new Seat(1, 3, true, false, false));
+        seats.add(new Seat(1, 4, true, false, false));
+        return seats;
     }
 
     /**
@@ -31,7 +48,9 @@ public class DataFactoryMock implements DataFactoryInterface {
      */
     public ObservableList<Flight> getFlights(String departureLocation, String arrivalLocation, String flightDate, Boolean mealService) {
         ObservableList<Flight> flights = FXCollections.observableArrayList();
-
+        flights.add(new Flight(1, "REY", "AEY", "11:00", "13:00", "2021-01-01", 15000, "Iceland Air", true));
+        flights.add(new Flight(2, "REY", "EGS", "11:00", "14:00", "2021-01-01", 22500, "Flugfélag Íslands", true));
+        flights.add(new Flight(3, "REY", "IFJ", "12:00", "14:00", "2021-01-01", 13500, "Flugfélag Íslands", true));
         return flights;
     }
 }
--- src/test/FlightSearchControllerTest.java
+++ src/test/FlightSearchControllerTest.java
@@ -1,19 +1,47 @@
 package test;
 
+import flightpackage.Flight;
+import flightpackage.FlightSearchController;
+import javafx.collections.ObservableList;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class FlightSearchControllerTest {
+    private FlightSearchController search;
+    private ObservableList<Flight> flights;
 
     @Before
     public void setUp() {
+        search = new FlightSearchController();
     }
 
     @After
     public void tearDown() {
+        flights.removeAll();
     }
 
     @Test
-    public void searchByAttributeTest() {
+    public void searchByAttributeTest1() {
+        flights = search.searchByAttribute("REY", null, null, null);
+        assertEquals(5, flights.size());
+    }
+
+    @Test
+    public void searchByAttributeTest2() {
+        flights = search.searchByAttribute(null, null, null, true);
+        assertEquals(6, flights.size());
+    }
+
+    @Test
+    public void searchByAttributeTest3() {
+        Flight f = new Flight( 1,"REY", "AEY", "11:00", "13:00", "2021-01-01", 15000, "Iceland Air", true);
+        flights = search.searchByAttribute("REY", "AEY", "2021-01-01", true);
+        assertEquals(f.getId(), flights.get(0).getId());
+    }
+
+    @Test
+    public void searchByAttributeTest4() {
+        flights = search.searchByAttribute("REY", "REY", null, null);
+        assertEquals(0, flights.size());
     }
 }
