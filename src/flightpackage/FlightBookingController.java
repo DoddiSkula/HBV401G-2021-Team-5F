13937b138457724a6fde22fcdc0f5b01a352c774
Tht40 <Tht40@users.noreply.github.com>
2021-04-09 14:37:58 +0000

    Update FlightBookingController.java

--- src/flightpackage/FlightBookingController.java
+++ src/flightpackage/FlightBookingController.java
@@ -19,13 +19,5 @@ public class FlightBookingController {
         // TODO
         return null;
     }
-    public void backbuttonPushed(ActionEvent event) throws IOException {
-        Parent tableViewParent = FXMLLoader.load(getClass().getResource("search.fxml"));
-        Scene tableViewScene = new Scene(tableViewParent);
-
-        //This line gets the Stage information
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(tableViewScene);
-        window.show();
-    }
+   
 }
