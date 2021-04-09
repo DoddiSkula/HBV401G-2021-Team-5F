a78f7ee7c3530f6101dd4b6b6696cb8071d96889
Tht40 <Tht40@users.noreply.github.com>
2021-04-09 14:11:48 +0000

    seats update

--- src/flightpackage/FlightBookingController.java
+++ src/flightpackage/FlightBookingController.java
@@ -1,10 +1,31 @@
 package flightpackage;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class FlightBookingController {
 
     // createBooking
     public Booking createBooking(User user, int flight_id, int seat_id) {
         // TODO
         return null;
+    }
+    public void backbuttonPushed(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("search.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
     }
 }
