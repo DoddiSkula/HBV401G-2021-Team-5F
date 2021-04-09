b0ef99e94eefb502daf132f82d4b554407aee6fd
Doddi <doddiskula@gmail.com>
2021-04-09 13:34:26 +0000

    booking control

--- src/flightpackage/FlightBookingController.java
+++ src/flightpackage/FlightBookingController.java
@@ -1,10 +1,15 @@
 package flightpackage;
 
 public class FlightBookingController {
+    private FlightSearchController searchController = new FlightSearchController();
+    private FlightUserController userController = new FlightUserController();
+    private DataFactory dataFactory = new DataFactory();
 
     // createBooking
-    public Booking createBooking(User user, int flight_id, int seat_id) {
+    public Booking createBooking(User user, Flight flight, int[] seat_id) {
         // TODO
         return null;
     }
+
+
 }
